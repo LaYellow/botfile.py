46caf788b3b31b11a05976486a12164a255187d8
LaYellow <48283030+LaYellow@users.noreply.github.com>
2019-03-09 21:02:47 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+from discord.ext.commands import has_permissions
+import asyncio
+from itertools import cycle
+import time
+import youtube_dl
+
+my_token = 'NTQyNTAyNjIzNjY4OTk0MDY5.DzvCIw.JoOolByOgGw3OqT4pnOMf5Xc5E8'
+
+client = commands.Bot(command_prefix = '/')
+
+client.remove_command('help')
+status = ['/help for commands', 'with LaYellow#9207', "LaYellow Bot"]
+
+players = {}
+
+
+async def change_status():
+    await client.wait_until_ready()
+    msgs = cycle(status)
+
+    while not client.is_closed:
+        current_status = next(msgs)
+        await client.change_presence(game=discord.Game(name =current_status))
+        await asyncio.sleep(10)
+
+@client.event
+async def on_ready():
+    print('The bot is online and is connected to discord')
